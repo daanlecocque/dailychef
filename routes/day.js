1382a0a91c204bce5e9001ecb1ebb1079b4866ab
8bart <b.w.m.v.rooijen@student.tue.nl>
2016-01-08 16:35:14 +0100

    day

--- routes/day.js
+++ routes/day.js
@@ -2,10 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/day', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('day', { title: 'Express' });
 });
+
+
+
 
 module.exports = router;
 
 //random
+var con = require('./../connection');
+function getSQL(callback) {
+    var query = con.query('SELECT * FROM sql4102332.attendees, sql4102332.users WHERE sql4102332.attendees.userid =sql4102332.users.userid', function (err, results, fields) {
+        if (err)
+            throw err;
+        // `results` is an array with one element for every statement in the query:
+        //console.log('The query-result is: ', results[1]);
+        var arr=[];
+
+        for (var i = 0; i < results.length; i++) {
+            arr[i] = results[i];
+            console.log(results[i]);
+        }
+        callback(arr);
+        // console.log(arr);
+
+
+        // con.end();
+    })
+}
+
+
+getSQL(function (arr) {
+
+});
