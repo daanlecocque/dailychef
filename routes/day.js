a3853052c5bcc2ae2e7a71c57705912044b1de90
8bart <b.w.m.v.rooijen@student.tue.nl>
2016-01-10 15:37:15 +0100

    fill table from database data

--- routes/day.js
+++ routes/day.js
@@ -11,29 +11,3 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-//random
-var con = require('./../connection');
-function getSQL(callback) {
-    var query = con.query('SELECT * FROM sql4102332.attendees, sql4102332.users WHERE sql4102332.attendees.userid =sql4102332.users.userid', function (err, results, fields) {
-        if (err)
-            throw err;
-        // `results` is an array with one element for every statement in the query:
-        //console.log('The query-result is: ', results[1]);
-        var arr=[];
-
-        for (var i = 0; i < results.length; i++) {
-            arr[i] = results[i];
-            console.log(results[i]);
-        }
-        callback(arr);
-        // console.log(arr);
-
-
-        // con.end();
-    })
-}
-
-
-getSQL(function (arr) {
-
-});
